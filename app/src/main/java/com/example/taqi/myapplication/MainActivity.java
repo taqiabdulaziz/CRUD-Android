e727cb6ecc8bd24aca84815d9b71e2c431a3edfa
taqiabdulaziz <svx.annexiv@gmail.com>
2018-12-26 22:18:21 +0700

    add AsyncTask

--- app/src/main/java/com/example/taqi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/taqi/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -43,50 +45,84 @@ import static org.apache.http.params.CoreConnectionPNames.CONNECTION_TIMEOUT;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<String> usernameList = new ArrayList<>();
+    private ArrayList<String> passwordList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Context context;
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://taqi6.localhost.run/users";
-        final TextView textView = findViewById(R.id.tv);
-        StringRequest stringRequest = new StringRequest(url, new com.android.volley.Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.i("KONTOL", response);
-                textView.setText(response);
-            }
-        }, new com.android.volley.Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.i("KONTOL", String.valueOf(error));
-
-            }
-        });
-
-
-        stringRequest.setRetryPolicy(new RetryPolicy() {
-            @Override
-            public int getCurrentTimeout() {
-                return 50000;
-            }
-
-            @Override
-            public int getCurrentRetryCount() {
-                return 50000;
-            }
-
-            @Override
-            public void retry(VolleyError error) throws VolleyError {
-
-            }
-        });
-
-        queue.add(stringRequest);
+        new getData().execute();
     }
 
+    private class getData extends android.os.AsyncTask<String, String, String> {
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.i("data1",String.valueOf(s));
+            try {
+                JSONArray data = new JSONArray(s);
+                JSONObject obj = data.getJSONObject(0);
+                Log.i("HASIL", obj.getString("username"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            RecyclerView recyclerView = findViewById(R.id.recyclerView);
+            RecyclerViewAdapter adapter = new RecyclerViewAdapter(usernameList, passwordList, getApplicationContext());
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            final String[] result = new String[1];
+            Context context = getApplicationContext();
+            RequestQueue queue = Volley.newRequestQueue(context);
+            String url = "http://taqi.localhost.run/users";
+            final TextView textView = findViewById(R.id.tv);
+            StringRequest stringRequest = new StringRequest(url, new com.android.volley.Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("KONTOL", response);
+                    result[0] = response;
+
+                }
+            }, new com.android.volley.Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.i("KONTOL", String.valueOf(error));
+
+                }
+            });
+
+
+            stringRequest.setRetryPolicy(new RetryPolicy() {
+                @Override
+                public int getCurrentTimeout() {
+                    return 50000;
+                }
+
+                @Override
+                public int getCurrentRetryCount() {
+                    return 50000;
+                }
+
+                @Override
+                public void retry(VolleyError error) throws VolleyError {
+
+                }
+            });
+            usernameList.add("w");
+            passwordList.add("a");
+            queue.add(stringRequest);
+            return String.valueOf(result);
+        }
+    }
 
 }
+
+
 
 
---
+++ app/src/main/java/com/example/taqi/myapplication/RecyclerViewAdapter.java
@@ -0,0 +1,59 @@
+package com.example.taqi.myapplication;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+    private ArrayList<String> usernameList = new ArrayList<>();
+    private ArrayList<String> passwordList = new ArrayList<>();
+    private Context context;
+
+    public RecyclerViewAdapter(ArrayList<String> usernameList, ArrayList<String> passwordList, Context context) {
+        this.usernameList = usernameList;
+        this.passwordList = passwordList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater layoutInflater = null;
+        View view = layoutInflater.from(context).inflate(R.layout.listitem, viewGroup, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.username.setText(usernameList.get(i));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return usernameList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView username,password;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            username = itemView.findViewById(R.id.tvUsername);
+            password = itemView.findViewById(R.id.tvPassword);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
